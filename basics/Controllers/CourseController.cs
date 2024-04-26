8ca503ce1f8c22e95eafd246c3ed1575f22310d3
Metehan Dündar <bir_kalem049@hotmail.com>
2024-04-26 20:53:19 +0300

    updates

--- basics/Controllers/CourseController.cs
+++ basics/Controllers/CourseController.cs
@@ -12,6 +12,7 @@ public class CourseController : Controller
 
         kurs.ID = 1;
         kurs.Title = "METEHAN";
+        kurs.Image = "1.JPG";
         return View(kurs);
     }
 
@@ -19,9 +20,9 @@ public class CourseController : Controller
     {
         var kurslar = new List<Course>()
         {
-            new Course() {ID = 1 ,Title = "Metehan"},
-            new Course() {ID = 2 ,Title = "Betüş"},
-            new Course() {ID = 3 ,Title = "Tufan"}
+            new Course() {ID = 1 ,Title = "Metehan", Image="1.JPG"},
+            new Course() {ID = 2 ,Title = "Betüş", Image="2.JPG"},
+            new Course() {ID = 3 ,Title = "Askim", Image="3.JPG"}
         };
         return View("CourseList", kurslar);
     }
--- basics/Program.cs
+++ basics/Program.cs
@@ -15,11 +15,8 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
